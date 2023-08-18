93d53f075462f4e94fda82d9eff10568886aac27
Felipe Basso <felipe.basso@techer.com.br>
2023-08-18 15:57:22 -0300

    formatting app spec

--- SRC/src/app/app.component.spec.ts
+++ SRC/src/app/app.component.spec.ts
@@ -2,24 +2,25 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-		AppComponent
-      ],
-    }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [
+				AppComponent
+			],
+		}).compileComponents();
+	});
 
-  it(`should have as title 'valoran-tournament'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('valoran-tournament');
-  });
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.componentInstance;
+		expect(app).toBeTruthy();
+	});
+
+	it(`should have as title 'valoran-tournament'`, () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.componentInstance;
+		expect(app.title).toEqual('valoran-tournament');
+	});
 
 });
